fc739fc47708b6020648f00c59112f71c066496a
amirgh82 <amirghhastam@gmail.com>
2023-10-17 14:02:55 +0330

    create project corporate-site

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+
+
+
+
+function App() {
+  return;
+  <>
+    <div className="app">
+
+    </div>
+  </>;
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./Navbar.css";
+export default function Navbar() {
+  return <div>Navbar</div>;
+}
