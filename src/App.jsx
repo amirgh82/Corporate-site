9a8cba3a7f4cbe67d53cf6c306366877b27211d5
amirgh82 <amirghhastam@gmail.com>
2023-10-17 14:20:12 +0330

    background header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
 import "./App.css";
-
-
-
-
+import Header from "./components/Header/Header";
 
 function App() {
-  return;
-  <>
-    <div className="app">
-
-    </div>
-  </>;
+  return (
+    <>
+      <div className="app">
+        <Header />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+import Navbar from "../Navbar/Navbar";
+
+export default function Header() {
+  return (
+    <header>
+      <Navbar />
+    </header>
+  );
+}
