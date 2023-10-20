da75858afb39f84b7c805763436dc8762de5065c
amirgh82 <amirghhastam@gmail.com>
2023-10-20 11:52:59 +0330

    delete Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <div className="app">
-        <Header />
+        <header>
+          <Navbar />
+        </header>
       </div>
     </>
   );
